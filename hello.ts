2a3a50ab78bc42ed4e6374c6591a0a09609c358f
J.s Ibitoye <jsibitoye@outlook.com>
2022-06-23 13:33:54 +0100

    dtype

--- hello.ts
+++ hello.ts
@@ -3,5 +3,9 @@ function greetPerson(person){
    return "hello " + person;  
 }
 
-let user = " joshua";
+let user: String = " joshua";
+
+//let varName : type = value;
+user = "4";
+
 console.log(greetPerson(user));
