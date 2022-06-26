c085deff3e85e0ee3959187a232835837608af2c
J.s Ibitoye <jsibitoye@outlook.com>
2022-06-26 07:35:01 +0100

    25th p

--- dist/index.js
+++ dist/index.js
@@ -5,4 +5,16 @@ if (num > 7)
 let josh = "Ibitoye Joshua";
 josh.concat("Oluwaseun");
 console.log(josh);
+class ModeUtil {
+    static toString(mode) {
+        return "josh is here again";
+    }
+}
+var en;
+(function (en) {
+    en[en["a"] = 1] = "a";
+    en[en["b"] = 2] = "b";
+    en[en["c"] = 3] = "c";
+})(en || (en = {}));
+alert(ModeUtil.toString(9));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,31 @@ let josh: string = "Ibitoye Joshua";
 josh.concat("Oluwaseun");
 
 console.log(josh);
+class ModeUtil {
+    public static toString(mode: any) : String{
+        return "josh is here again";
+    }
+}
+
+
+enum en {
+
+    a = 1,b,c,
+    // d = ()=>{
+    //     return 0;}
+   
+}
+
+alert(ModeUtil.toString(9));
+
+namespace Mode {
+    // export function toString(mode: Mode): string {
+    //     return Mode[mode];
+    // }
+
+    // export function parse(mode: string): Mode {
+    //     return Mode[mode];
+    // }
+}
+
+
